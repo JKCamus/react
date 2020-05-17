2d42cf964cc8a51601634eae5ce4ff69f62a25e1
JKCamus <924850758@qq.com>
2020-05-17 19:03:16 +0800

    可输入代码块

--- react/wow-react/src/Comment.jsx
+++ react/wow-react/src/Comment.jsx
@@ -16,18 +16,16 @@ class Comment extends React.Component {
   componentWillMount() {
     this._updateTimeString();
     // 每5秒更新一次时间戳
-    this._timer = setInterval(this._updateTimeString.bind(this), 5000);
+    this._timer = setInterval(this._updateTimeString.bind(this), 20000);
   }
-  componentWillUnmount(){
-    clearInterval(this._timer)
-    
+  componentWillUnmount() {
+    clearInterval(this._timer);
   }
   handleDeleteComment = () => {
     console.log("点击删除");
     if (this.props.onDeleteComment) {
       this.props.onDeleteComment(this.props.index);
       // console.log(this.props.index);
-      
     }
   };
   /* 添加时间戳，两次评论的间隔时间 */
@@ -41,15 +39,30 @@ class Comment extends React.Component {
           : `${Math.round(Math.max(duration, 1))}秒前`,
     });
   }
+  _getProcessedContent(content) {
+    return content
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;")
+      .replace(/`([\S\s]+?)`/g, "<code>$1</code>");
+  }
   render() {
+    const comment = this.props.comment;
     return (
       <div>
         <div className="comment">
           <div className="comment-user">
-            <span className="comment-username">{this.props.comment.username} </span>：
+            <span className="comment-username">{comment.username} </span>：
           </div>
-          <p> {this.props.comment.content}</p>
-          
+          {/* <p> {comment.content}</p> */}
+          <p
+            dangerouslySetInnerHTML={{
+              __html: this._getProcessedContent(comment.content),
+            }}
+          ></p>
+
           <span className="comment-createdtime">{this.state.timeString}</span>
         </div>
         <span className="commentdelete" onClick={this.handleDeleteComment}>
