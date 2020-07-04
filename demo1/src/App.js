d911a869b9f79c85fe8faeaf88914d4fe6bff8fa
JKCamus <924850758@qq.com>
2020-07-04 22:06:58 +0800

    HOC_enhance_props

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import HOCEnhance from './HOC_enhance_props'
 
 export class App extends Component {
   render() {
     return (
       <div>
         <h1>hello everyone!</h1>
+        <HOCEnhance></HOCEnhance>
       </div>
     )
   }
---
+++ 关于Boolean和非空判断.md
@@ -0,0 +1,15 @@
+# 关于Boolean和非空判断
+
+### 关于对象
+
+`Object.keys(obj)`得出的是数组。
+
+Object.keys(obj).length
+
+### 关于数组
+
+通过判断他的长度
+
+array.length
+
+### 
